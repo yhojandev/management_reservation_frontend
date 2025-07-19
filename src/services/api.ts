1a1971f9bcfa50ea45a343d7558c4528da994ced
yhojan <yhojan5416@gmail.com>
2025-07-18 19:29:02 -0500

    cambios en la peticiones de la API, obtencion del token

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,14 +2,23 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+// Interceptor de solicitud: Añade el token de acceso a cada request
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken"); // Obtén el token del localStorage
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`; // Añade el token al encabezado
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-});
+);
 
 export default api;
